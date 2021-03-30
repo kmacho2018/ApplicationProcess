033d77eea9bf8548f8a4c876cecc550447ac66fb
DESKTOP-2G1L6CA\Admin <juancarloscamacho01@gmail.com>
2021-03-29 20:11:27 -0400

    Adding Asset Model, add FluentValidation package, Update SwaggerUI Packages

---
+++ Hahn.ApplicationProcess.Application/Hahn.ApplicationProcess.February2021.Domain/Models/Asset.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hahn.ApplicationProcess.February2021.Domain.Models
+{
+    class Asset
+    {
+        public int Id { get; set; }
+        public string AssetName  { get; set; }
+        public enum Department { HQ, Store1, Store2, Store3, MaintenanceStation }
+        public string CountryOfDepartment { get; set; }
+        public string EMailAdressOfDepartment { get; set; }
+        public DateTime PurchaseDate { get; set; }
+        public bool Broken { get; set; }
+    }
+}
